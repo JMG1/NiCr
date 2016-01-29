972c91737a3e72642d684524f585d3a90142b15a
JMG1 <asaber@gmx.com>
2016-01-29 21:34:39 +0100

    added some colors

--- Workbench/NiCrSerial.py
+++ Workbench/NiCrSerial.py
@@ -10,9 +10,12 @@ class ControlPanel:
     def __init__(self):
         # variables
         self.serial_up = False
+        self.palette = QtGui.QPalette()
+        # palette.setColor(QtGui.QPalette.Foreground,QtCore.Qt.red)
         # load ui Window
         ui_w = FreeCADGui.UiLoader()
         self.w = ui_w.load(__dir__ + '/ui/ControlPanel.ui')
+        # set labels
         # connnect window buttons
         self.w.btn_refresh.clicked.connect(self.connectToMachine)
         self.w.btn_send_serial_command.clicked.connect(self.sendSerialCommand)
@@ -32,11 +35,15 @@ class ControlPanel:
             time.sleep(0.1)
             try:
                 self.serial = serial.Serial(self.port_address, 115200, timeout = 1.0)
+                self.palette.setColor(QtGui.QPalette.Foreground,QtCore.Qt.green)
+                self.w.lb_serial_status.setPalette(self.palette)
                 self.w.lb_serial_status.setText('CONNECTED')
                 self.serial_up = True
                 break
 
             except serial.SerialException:
+                self.palette.setColor(QtGui.QPaltete.Foreground,QtCore.Qt.red)
+                self.w.lb_serial_status.setPalette(self.palette)
                 self.w.lb_serial_status.setText('Failed to connect')
                 self.serial_up = False
 
@@ -63,12 +70,20 @@ class ControlPanel:
                                                      'Load .nicr file:',
                                                      '/home')
         nicr_file = open(file_dir[0], 'r')
+        self.w.setWindowTitle('NiCr Control Panel: ' + file_dir[0])
         self.readCommandsToMachine(nicr_file)
 
     def readCommandsToMachine(self, nicr_file):
         line = ''
         n = 0
-        while line != 'INIT' and n < 1000:
+        while line != 'INIT' and n < 10000:
             line = nicr_file.readline()
-            self.w.lw_commands.addItem(line)
+            if line.split(':')[0] == 'PATH NAME':
+                self.w.lbl_file_name.setText(line.split(':')[1])
+
+            line_number = str(n) + '|'
+            for i in xrange(10 - len(str(n) + '|')):
+                line_number += ' '
+
+            self.w.lw_commands.addItem(line_number + line[:-1])
             n += 1
